60dbf896316b1070c8608aebb16371ec8a335b3b
Scott Davis <scott@thirstyhead.com>
2020-09-23 14:42:19 -0600

    added playback for recording

--- components/audio-recorder/index.js
+++ components/audio-recorder/index.js
@@ -12,8 +12,7 @@ window.customElements.define('audio-recorder',
       let startStopButton = this.shadowRoot.querySelector('#start-stop');
       startStopButton.addEventListener('click', (event) => this.startStopClickHandler(event) );
 
-      this.audioChunks = [];
-      this.mimeType = 'audio/aac';
+      this.mediaRecorder = undefined;
       this.isRecording = false;
    }
 
@@ -30,7 +29,6 @@ window.customElements.define('audio-recorder',
     get content(){
       let content = document.createElement('div');
       content.innerHTML = `
-      <audio id="audio-recorder" ></audio>
       <button id="start-stop">Start Recording</button>
       `;
       return content;
@@ -50,19 +48,27 @@ window.customElements.define('audio-recorder',
       startStopButton.classList.add('stop');
       this.isRecording = true;
 
-      this.mediaSource = new MediaSource();
-      this.mediaSource.addEventListener('sourceopen', (event) => this.sourceOpenHandler(event));
-      this.sourceBuffer = undefined;
+      let audioChunks = [];
 
       const constraints = {
         audio: true,
         video: false
       };
+
+      const options = {
+        type: 'audio/aac'
+      }
         
       navigator.mediaDevices.getUserMedia(constraints)
         .then( (stream) => {
-          window.stream = stream;
-          this.shadowRoot.querySelector('audio').srcObject = stream;
+          this.mediaRecorder = new MediaRecorder(stream, options);
+          this.mediaRecorder.addEventListener('dataavailable', (event) => {
+            audioChunks.push(event.data);
+          });
+          this.mediaRecorder.addEventListener('stop', (event) => {
+            this.saveRecording(new Blob(audioChunks, options));
+          });
+          this.mediaRecorder.start();
         })
         .catch( (error) => {
           console.error(`navigator.getUserMedia error: ${error}`);
@@ -73,11 +79,19 @@ window.customElements.define('audio-recorder',
       let startStopButton = this.shadowRoot.querySelector('#start-stop');
       startStopButton.innerHTML = 'Start Recording';
       startStopButton.classList.remove('stop');
+      this.mediaRecorder.stop();
       this.isRecording = false;
     }
 
-    sourceOpenHandler(event){
+    saveRecording(audioBlob){
+      let blobUrl = URL.createObjectURL(audioBlob);
+      let audio = document.createElement('audio');
+      audio.setAttribute('src', blobUrl);
+      audio.setAttribute('controls', '');
+      this.shadowRoot.append(audio);
     }
+
+
   }
 
 
